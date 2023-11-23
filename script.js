dee79279817cb6421b8a8ca0a7519c04b2119205
tom4sdvorak <tom4s.dvorak@gmail.com>
2023-11-24 01:42:39 +0200

    Updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-// Loads a list of cinemas from API
+// Load list of cinemas from the API into cinema selector on the main page
 function getCinemas(){
+    console.log("RUnning dgetCinemas");
     var url = "https://www.finnkino.fi/xml/TheatreAreas/";
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
@@ -11,7 +12,17 @@ function getCinemas(){
             for(var i = 0; i < x.length; i++){
                 var item = document.createElement('option');
                 item.setAttribute("value", x[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue);
-                item.innerHTML = x[i].getElementsByTagName("Name")[0].childNodes[0].nodeValue;
+                if (i == 0){
+                    item.setAttribute("selected", true);
+                }
+                var name = x[i].getElementsByTagName("Name")[0].childNodes[0].nodeValue;
+                if (name == "Valitse alue/teatteri"){
+                    name = "All Cinemas";
+                }
+                else if (name == "Pääkaupunkiseutu"){
+                    name = "Capitol Area"
+                }
+                item.innerHTML = name;
                 document.getElementById("cinemaList").appendChild(item);
             }
             document.getElementById("cinemaList").addEventListener("change", displayMovies);
@@ -19,7 +30,9 @@ function getCinemas(){
       };
 }
 
+// Load list of dates the API can be accessed for movies into date selector on the main page
 function getDates(){
+    console.log("RUnning getDates");
     // Get list of available schedule dates from API
     var url = "https://www.finnkino.fi/xml/ScheduleDates/";
     var xhttp = new XMLHttpRequest();
@@ -30,25 +43,25 @@ function getDates(){
             var xmlDoc = this.responseXML;
             var x = xmlDoc.getElementsByTagName("dateTime");
             for(var i = 0; i < x.length; i++){
-                console.log("Adding date: " + x[i].childNodes[0].nodeValue);
                 var item = document.createElement('option');
                 var date = new Date(x[i].childNodes[0].nodeValue);
                 item.setAttribute("value", date.toLocaleDateString());
+                if (i == 0){
+                    item.setAttribute("selected", true);
+                }
                 item.innerHTML = date.toLocaleDateString();
                 document.getElementById("dateList").appendChild(item);
             }
             document.getElementById("dateList").addEventListener("change", displayMovies);
-        }
+        }    
     }
 }
 
-// Displays schedule for today for selected cinema
+// Displays list of movies
 function displayMovies(){
-    // Check selected cinema and date
+    // Check for selected cinema and date and call API to get the specific schedule
     var cinemaID = document.getElementById("cinemaList").value;
     var dateOfMovies = document.getElementById("dateList").value.replace(/\//g,".");
-
-    console.log("Looking for movies from cinema " + cinemaID + " and on date " + dateOfMovies);
     var url = "https://www.finnkino.fi/xml/Schedule/?area=" + cinemaID + "&dt=" + dateOfMovies;
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
@@ -60,39 +73,64 @@ function displayMovies(){
     });
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log("Got data from API under url " + url);
+            
+            // Once we have received XML data from API, we separate them by <Show> tag name
             var xmlDoc = this.responseXML;
             var x = xmlDoc.getElementsByTagName("Show");
             var movieContainer = document.getElementById("movieSelection");
             movieContainer.innerHTML = "";
+            
             // Create element with the selected date and append it to beginning of div container
             dateHeading = document.getElementById("date");
             dateHeading.innerHTML = dateOfMovies;
-
+            
+            // Loop thru our list of <Show> from API
             for(var i = 0; i < x.length; i++){
                 // Create div container for the movie
                 var item = document.createElement('div');
                 item.setAttribute("class", "movie");
 
-                // Create img of the movie
+                // Create div holding img of the movie
+                var picDiv = document.createElement('div');
                 var pic = document.createElement('img');
-                pic.setAttribute("src", x[i].getElementsByTagName("EventSmallImagePortrait")[0].childNodes[0].nodeValue);
-                item.appendChild(pic);
+                picDiv.setAttribute("class", "moviePic");
+                
+                // IF checking to get available portrait image (prioty: medium>large>small) because not all Shows in API have same sizes available
+                if(x[i].getElementsByTagName("EventMediumImagePortrait").length > 0){
+                    pic.setAttribute("src", x[i].getElementsByTagName("EventMediumImagePortrait")[0].childNodes[0].nodeValue);
+                }
+                else if(x[i].getElementsByTagName("EventLargeImagePortrait").length > 0){
+                    pic.setAttribute("src", x[i].getElementsByTagName("EventMediumImagePortrait")[0].childNodes[0].nodeValue);
+                }
+                else{
+                    pic.setAttribute("src", x[i].getElementsByTagName("EventSmallImagePortrait")[0].childNodes[0].nodeValue);
+                }
+        
+                picDiv.appendChild(pic);
+                item.appendChild(picDiv);
 
                 // Create elements that hold information about the movie
                 var info = document.createElement('div');
+                info.setAttribute("class", "movieInfo");
                 var title = document.createElement('h2');
                 title.innerHTML = x[i].getElementsByTagName("OriginalTitle")[0].childNodes[0].nodeValue + " (" + x[i].getElementsByTagName("ProductionYear")[0].childNodes[0].nodeValue + ")";
                 var startTime = document.createElement('h4');
+                
+                // Get movie showing start and convert it to hh:mm format
                 var date = new Date(x[i].getElementsByTagName("dttmShowStart")[0].childNodes[0].nodeValue);
                 startTime.innerHTML = date.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+                
                 var other = document.createElement('p');
                 len = x[i].getElementsByTagName("LengthInMinutes")[0].childNodes[0].nodeValue;
                 other.innerHTML = "Length: " + len + " min";
+                
+                // Check if there is Spoken Language listed and if so, translate the ISO code of the language to english term
                 if(x[i].getElementsByTagName("SpokenLanguage").length > 0){
                     lan = dnStd.of(x[i].getElementsByTagName("SpokenLanguage")[0].getElementsByTagName("ISOTwoLetterCode")[0].childNodes[0].nodeValue);
                     other.innerHTML += "<br>Language: " + lan;
                 }
+
+                // Default to None subtitles, look if there are (one or two) subtitles for the movie listed and translate the ISO code of the language to english term
                 var sub = "None";
                 if(x[i].getElementsByTagName("SubtitleLanguage1").length > 0){
                     sub = dnStd.of(x[i].getElementsByTagName("SubtitleLanguage1")[0].getElementsByTagName("ISOTwoLetterCode")[0].childNodes[0].nodeValue);
@@ -100,12 +138,12 @@ function displayMovies(){
                 if(x[i].getElementsByTagName("SubtitleLanguage2").length > 0){
                     sub = sub + ", " + dnStd.of(x[i].getElementsByTagName("SubtitleLanguage2")[0].getElementsByTagName("ISOTwoLetterCode")[0].childNodes[0].nodeValue);
                 }
+                
                 other.innerHTML += "<br>Subtitles: " + sub;
                 info.appendChild(title);
                 info.appendChild(startTime);
                 info.appendChild(other);
                 item.appendChild(info);
-                // Append the new div to div container
                 movieContainer.appendChild(item);
 
             }
